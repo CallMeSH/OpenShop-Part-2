db9d9fde058a314b660de491ef44ac62c7b6ff6c
Charles-Henri DUMALIN <dumalin.ch@gmail.com>
2014-07-21 21:15:36 +0200

    Initial Commit

---
+++ OpenShop/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OpenShop
+//
+//  Created by Charles-Henri Dumalin on 21/07/2014.
+//  Copyright (c) 2014 Charles-Henri Dumalin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
